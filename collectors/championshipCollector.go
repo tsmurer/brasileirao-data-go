ee20cb2af9ce3f64f9af11a64c66a84391491b7b
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-22 23:31:16 -0300

    removing unnecessary comments and spaces

--- collectors/championshipCollector.go
+++ collectors/championshipCollector.go
@@ -1,8 +1,6 @@
 package collector
 
 import (
-	"log"
-
 	"github.com/gocolly/colly"
 )
 
@@ -19,33 +17,25 @@ func (c ChampionshipCollector) Collect() {
 	collector := CreateCollector(c.Url)
 	teamPages := [20]TeamPage{}
 	collector.OnHTML("table", func(e *colly.HTMLElement) {
-		// Initialize an index
 		index := 0
-
-		// Find and iterate through table rows
 		e.ForEach("tbody tr", func(_ int, el *colly.HTMLElement) {
-			// Skip the first row (table header)
 			if index < 20 {
-				// Extract club name and link from the table data
 				name := el.ChildAttr("td:nth-child(2) a", "title")
 				link := el.ChildAttr("td:nth-child(2) a", "href")
-
-				// Add the club to the fixed-size array
 				teamPages[index] = TeamPage{
 					name, link,
 				}
-
-				// Increment the index
 				index++
 			}
 		})
 	})
 
 	collector.Visit(c.Url)
-	for i := 0; i < 20; i++ {
-		log.Println("ARRAY:")
-		log.Println(i, ": ", teamPages[i])
-	}
+	// write test for this instead of the code below
+	// for i := 0; i < 20; i++ {
+	// 	log.Println("ARRAY:")
+	// 	log.Println(i, ": ", teamPages[i])
+	// }
 }
 func CollectChampionship() {
 	var championshipCollector CollectorInterface = ChampionshipCollector{CHAMPIONSHIP_PAGE_URL}
