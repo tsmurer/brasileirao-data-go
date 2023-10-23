c85ac8c7d5fb667b2be56eff3a7ab31bd3981ba4
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-23 14:27:46 -0300

    added tests for championCollector

--- collectors/championshipCollector.go
+++ collectors/championshipCollector.go
@@ -1,11 +1,14 @@
 package collector
 
 import (
+	"log"
+
 	"github.com/gocolly/colly"
 )
 
-type ChampionshipCollector struct {
-	Url string
+type ChampionshipCollectorInterface interface {
+	CollectorInterface
+	GetTeamPages() [20]TeamPage
 }
 
 type TeamPage struct {
@@ -13,26 +16,38 @@ type TeamPage struct {
 	Url  string
 }
 
-func (c ChampionshipCollector) Collect() {
+type ChampionshipCollector struct {
+	Url       string
+	TeamPages [20]TeamPage
+}
+
+func (c *ChampionshipCollector) Collect() {
 	collector := CreateCollector(c.Url)
-	teamPages := [20]TeamPage{}
 	collector.OnHTML("table", func(e *colly.HTMLElement) {
 		index := 0
 		e.ForEach("tbody tr", func(_ int, el *colly.HTMLElement) {
 			if index < 20 {
 				name := el.ChildAttr("td:nth-child(2) a", "title")
 				link := el.ChildAttr("td:nth-child(2) a", "href")
-				teamPages[index] = TeamPage{
+				c.TeamPages[index] = TeamPage{
 					name, link,
 				}
 				index++
 			}
 		})
 	})
-
 	collector.Visit(c.Url)
 }
+
+func (c ChampionshipCollector) GetTeamPages() [20]TeamPage {
+	return c.TeamPages
+}
+
 func CollectChampionship() {
-	var championshipCollector CollectorInterface = ChampionshipCollector{CHAMPIONSHIP_PAGE_URL}
+	var championshipCollector ChampionshipCollectorInterface = &ChampionshipCollector{CHAMPIONSHIP_PAGE_URL, [20]TeamPage{}}
 	championshipCollector.Collect()
+	log.Println("CollectChampionship()")
+	for _, page := range championshipCollector.GetTeamPages() {
+		log.Println(page)
+	}
 }
--- collectors/championshipCollector_test.go
+++ collectors/championshipCollector_test.go
@@ -1,7 +1,50 @@
 package collector
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestChampionshipCollectorCollectImplementation(t *testing.T) {
+	"github.com/tsmurer/brasileirao-data/assets"
+)
 
+func setupSuite() {
+
+}
+
+func TestChampionshipCollector_Collect(t *testing.T) {
+	embeddedHTML := assets.Assets
+
+	htmlData, err := embeddedHTML.ReadFile("championshipPage.html")
+	if err != nil {
+		t.Fatalf("Error reading embedded file: %v", err)
+	}
+
+	// Create a test HTTP server to serve the HTML content
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(htmlData))
+	}))
+	defer server.Close()
+
+	collector := &ChampionshipCollector{Url: server.URL, TeamPages: [20]TeamPage{}}
+	collector.Collect()
+
+	for _, page := range collector.TeamPages {
+		if page.Name == "" || page.Url == "" {
+			t.Errorf("Expected all indexes populated but found: { Name: %v, Url: %v }", page.Name, page.Url)
+		}
+	}
+}
+
+// Add more test functions for other aspects of your code, if necessary
+
+func TestChampionshipCollector_GetTeamPages(t *testing.T) {
+	collector := ChampionshipCollector{
+		TeamPages: [20]TeamPage{ /* Initialize with your test data */ },
+	}
+	result := collector.GetTeamPages()
+	if len(result) != 20 {
+		t.Errorf("Expected result length of 20, but got %d", len(result))
+	}
 }
